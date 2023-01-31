f4a17d68a5ab30f6a6e2e54d5cee5ba74cdae7b6
Markus Bille <43919485+MaxusTheOne@users.noreply.github.com>
2023-01-31 13:37:34 +0100

    Added Mail and Bg colour

--- app.js
+++ app.js
@@ -3,5 +3,7 @@ function sayHello() {
     "Hej " + document.querySelector("#name").value;
   document.querySelector("#ageT").textContent =
     "Du hævder du er " + document.querySelector("#age").value;
+  document.querySelector("#emailT").textContent =
+    "Og at din mail er " + document.querySelector("#email").value;
 }
 document.querySelector("button").addEventListener("click", sayHello);
